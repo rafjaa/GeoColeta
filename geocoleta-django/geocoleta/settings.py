ba78f8a3c814071bdc4ede7b3bfa2de53e686815
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-11 07:42:36 -0300

    adicionado: configuração correta do postgresql.

--- geocoleta-django/geocoleta/settings.py
+++ geocoleta-django/geocoleta/settings.py
@@ -16,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geoColeta',                      # Or path to database file if using sqlite3.
+        'NAME': 'geocoleta',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
