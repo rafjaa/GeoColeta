9f2502391e5c47cc160dd234b9aa51d88fb57562
Thiago <thiagodd.silva@gmail.com>
2013-07-06 16:26:29 -0300

    Novos Models criados (Local e Notícia).

---
+++ geocoleta/coletor_app/admin.py
@@ -0,0 +1,15 @@
+from models import TipoColetor
+from models import Coletor
+from models import Local
+from models import Noticia
+
+from django.contrib import admin
+
+class PostAdmin(admin.ModelAdmin):
+	class Media:
+		js = ('/static/js/tinymce/tinymce.min.js', '/static/js/tinymce/textarea.js')
+
+admin.site.register(Coletor, PostAdmin)
+admin.site.register(TipoColetor)
+admin.site.register(Local)
+admin.site.register(Noticia, PostAdmin)
--- geocoleta/coletor_app/models.py
+++ geocoleta/coletor_app/models.py
@@ -16,8 +16,8 @@ class TipoColetor(models.Model):
 class Coletor(models.Model):
 	latitude = models.CharField('Latitude', max_length=50)
 	longitude = models.CharField('Longitude', max_length=50)
-	descricao = models.TextField('Descrição')
 	tipo = models.ManyToManyField('TipoColetor')
+	descricao = models.TextField('Descrição')
 	
 	def __unicode__(self):
 		return u'{0}'.format(self.descricao)
@@ -38,4 +38,33 @@ class Registro(models.Model):
 		
 	class Meta():
 		verbose_name = u'Registro'
-		verbose_name_plural = u'Registros'	
+		verbose_name_plural = u'Registros'
+		
+#Avaliar se os próximos dois models podem fazer parte dessa app
+#ou é melhor criar uma nova app para eles.
+class Local(models.Model):
+	nome = models.CharField('Nome', max_length=100)
+	latitude = models.CharField('Latitude', max_length=50)
+	longitude = models.CharField('Longitude', max_length=50)
+	descricao = models.TextField('Descrição')
+	#Implementação futura.
+	#imagem = models.FileField()
+	
+	def __unicode__(self):
+		return u'{0}'.format(self.nome)
+	
+	class Meta():
+		verbose_name = u'Local'
+		verbose_name_plural = u'Locais'
+		
+class Noticia(models.Model):
+	titulo = models.CharField('Título', max_length=50)
+	corpo = models.TextField('Corpo')
+	data = models.DateTimeField(auto_now=True)
+	
+	def __unicode__(self):
+		return u'{0}'.format(self.titulo)
+		
+	class Meta():
+		verbose_name = u'Notícia'
+		verbose_name_plural = u'Notícias'
