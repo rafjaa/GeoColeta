4f81b6342ad88b8f0e3e64070c9cabcee12b17c3
Rafael Alencar <rafjaa@gmail.com>
2013-07-06 17:18:20 -0300

    Alteração para junção das duas apps

--- geocoleta/settings.py
+++ geocoleta/settings.py
@@ -135,7 +135,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'mobile_app',
-    'coletor_app',
 )
 
 # A sample logging configuration. The only tangible logging
