a1551504869a0ed4f3b044b8f67f1951daa0adc2
Rafael Alencar <rafjaa@gmail.com>
2013-10-27 17:23:33 -0200

    Migração para a versão 0.1.1

--- geocoleta/mobile_app/context_processors.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-from django.conf import settings
-
-def google_api_key(request):
-    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
-
-def institution(request):
-    return {'INSTITUTION': settings.INSTITUTION}
--- geocoleta/views.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
-def home(request):
-    return render_to_response('home.html')
