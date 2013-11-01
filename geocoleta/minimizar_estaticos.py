85522057aaf25c5da340749fcb1778bf018c7fd2
Rafael Alencar <rafjaa@gmail.com>
2013-11-01 14:03:32 -0200

    Compressão automática de CSS e JS implementada

--- geocoleta/minimizar_estaticos.py
+++ geocoleta/minimizar_estaticos.py
@@ -19,3 +19,37 @@ def minify(codigo, tipo):
 	req = urllib.urlopen(APIS[tipo], args)
 
 	return req.read()
+
+
+if __name__ == '__main__':
+
+	STATIC = './static'
+
+	# Arquivos JavaScript
+
+	src_jquery_min = open(STATIC + '/js/jquery-1.10.2.min.js').read()
+	src_script = open(STATIC + '/js/script.js').read()
+	
+	src_script_min = minify(src_script, 'js')
+
+	with open(STATIC + '/js/jquery.script.min.js', 'w') as f:
+		f.write(src_jquery_min + '\n\n' + src_script_min)
+
+
+	# Arquivos css
+
+	src_lungo_theme = open(STATIC + '/css/lungo/lungo.theme.css').read()
+	src_style = open(STATIC + '/css/style.css').read()
+	
+	src_lungo_theme_min = minify(src_lungo_theme, 'css')
+	src_style_min = minify(src_style, 'css')
+
+	with open(STATIC + '/css/lungo.theme.style.min.css', 'w') as f:
+		f.write(src_lungo_theme_min + '\n\n' + src_style_min)
+
+
+	src_lungo = open(STATIC + '/css/lungo/lungo.css').read()
+	src_lungo_min = minify(src_lungo, 'css')
+
+	with open(STATIC + '/css/lungo/lungo.min.css', 'w') as f:
+		f.write(src_lungo_min)
